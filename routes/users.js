8c96c9be83994ed61e4026b0ce9a230363dee8d0
fahmiin <fahmiin@tyne-solutions.com>
2020-06-04 00:38:30 +0800

    Added api endpoints for users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
-router.get('/register', (req, res) => res.send('You are regsitering as a user'));
-router.get('/auth', (req, res) => res.send('You are authenticating as a user'));
-router.get('/me', (req, res) => res.send('This is my profile'));
-router.get('/validate', (req, res) => res.send('Validating token'));
+router.post('/register', (req, res) => {
+  let userData = req.body;
+  let user = new User(userData);
+  user.save((err, user) => {
+    if (err) console.log(err);
+    else res.status(200).send(user);
+  });
+});
+
+router.post('/login', (req, res) =>{
+  let loginData = req.body
+  User.findOne({ email: loginData.email }, (err, user) => {
+    if (err) console.log(err);
+    else {
+      if (!user) res.status(401).send('Invalid email!');
+      else if (user.password !== loginData.password) res.status(401).send('Incorrect password!');
+      else res.status(200).send(user);
+    }
+  })
+});
 
 module.exports = router;
